c08d8611b0556e8eecc2f472fa8a081d2494284a
stevenlsh <steven@server2u.net>
2024-10-03 12:45:10 +0800

    Update __init__.py

--- de_school_library/models/__init__.py
+++ de_school_library/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import genre
-from . import res_company
+# from . import res_company
 from . import res_partner
 from . import product
 from . import product_product
